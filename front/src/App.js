246281edccdff19e6debc1619f19be0359b88d3d
bohyunkang <bbohyun@kakao.com>
2021-04-10 17:41:47 +0900

    add: 라우트 적용 #10

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,17 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import GlobalStyles from './components/common/GlobalStyles';
 
 function App() {
-  return <div className="App">Hello ReactJs!</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      <Route path='/' component={Home} exact={true} />
+      <Route path='/about' component={About} />
+    </div>
+  );
 }
 
 export default App;
