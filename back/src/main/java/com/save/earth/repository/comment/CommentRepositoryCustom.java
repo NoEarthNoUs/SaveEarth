8841022e13dd96c594626c6858bd30af36902b89
Hwanhee Kim <dsa1461@gmail.com>
2021-04-20 16:09:43 +0900

    update: CommentRepositoryCustom 쿼리문 수정

--- back/src/main/java/com/save/earth/repository/comment/CommentRepositoryCustom.java
+++ back/src/main/java/com/save/earth/repository/comment/CommentRepositoryCustom.java
@@ -3,7 +3,6 @@ package com.save.earth.repository.comment;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.save.earth.domain.Comment;
 import com.save.earth.domain.QComment;
-import com.save.earth.domain.QPlace;
 import com.save.earth.domain.QUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -16,14 +15,14 @@ public class CommentRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     public List<Comment> findComment(Long placeId){
-        QPlace qPlace = QPlace.place;
         QComment qComment = QComment.comment;
         QUser qUser = QUser.user;
 
-        return jpaQueryFactory.selectDistinct(qComment)
-                .from(qComment).where(qPlace.id.eq(placeId))
-                .innerJoin(qComment.placeComment, qPlace)
-                .innerJoin(qComment.userComment, qUser)
-                .fetch();
+        return jpaQueryFactory.select(qComment)
+            .from(qComment)
+            .innerJoin(qComment.userComment, qUser)
+            .fetchJoin()
+            .where(qComment.placeComment.id.eq(placeId))
+            .fetch();
     }
 }
