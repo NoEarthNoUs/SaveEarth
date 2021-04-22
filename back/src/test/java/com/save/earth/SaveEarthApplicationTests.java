3f864c5ce0fdc09c3ebaad308269a67d966da214
Hwanhee Kim <dsa1461@gmail.com>
2021-04-22 11:14:33 +0900

    chore: 테스트코드 수정

--- back/src/test/java/com/save/earth/SaveEarthApplicationTests.java
+++ back/src/test/java/com/save/earth/SaveEarthApplicationTests.java
@@ -1,13 +1,8 @@
 package com.save.earth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SaveEarthApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
