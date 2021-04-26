762f712e9777bba03066ad001e5983774f89107f
bohyunkang <bbohyun@kakao.com>
2021-04-26 14:12:46 +0900

    update: 더미 데이터 삭제

--- front/src/components/place/PlaceList.jsx
+++ front/src/components/place/PlaceList.jsx
@@ -48,62 +48,6 @@ const PlaceList = () => {
     );
   if (!places) return null;
 
-  // const placeData = [
-  //   {
-  //     id: 1,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  //   {
-  //     id: 3,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  //   {
-  //     id: 4,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  //   {
-  //     id: 5,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  //   {
-  //     id: 6,
-  //     name: '비건이즈',
-  //     address: '서울시 강남구 뭐뭐동 665-1',
-  //     imgUrl:
-  //       'http://www.shinailbo.co.kr/news/photo/201911/1221389_473186_3253.jpg',
-  //     category: '식당',
-  //     contents: '이 장소는 비건들이 자주 가는 식당입니다.',
-  //   },
-  // ];
   return (
     <Wrapper>
       <div className='place-list'>
