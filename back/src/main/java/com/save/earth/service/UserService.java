bd1d3fa50df49021a3b4f46f5cbbc182b8114593
Hwanhee Kim <dsa1461@gmail.com>
2021-04-26 23:09:10 +0900

    add: userId로 user 조회하기 #52

--- back/src/main/java/com/save/earth/service/UserService.java
+++ back/src/main/java/com/save/earth/service/UserService.java
@@ -1,10 +1,13 @@
 package com.save.earth.service;
 
+import com.save.earth.domain.User;
 import com.save.earth.dto.user.UserSaveRequestDto;
 import com.save.earth.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -16,5 +19,14 @@ public class UserService {
     public void saveUser(UserSaveRequestDto userSaveRequestDto){
         userSaveRequestDto.setId("123456789765432");
         userRepository.save(userSaveRequestDto.toEntity());
+    }
+
+    public String selectUser(String userId){
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent()){
+            return user.get().getId();
+        }else{
+            return null;
+        }
     }
 }
