95bd61683bd30d6b689edd95e23532d9f69f5c89
bohyunkang <bbohyun@kakao.com>
2021-05-05 03:13:35 +0900

    update: 페이지네이션 구현

--- front/src/pages/Place.jsx
+++ front/src/pages/Place.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { Header, Footer } from "../components/common";
-import { PlaceList, PlaceSearch } from "../components/place";
-import { subPageTitle } from "../styles/mixins";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Header, Footer, Pagination } from '../components/common';
+import { PlaceList, PlaceSearch } from '../components/place';
+import { subPageTitle } from '../styles/mixins';
+import axios from 'axios';
 
 // 플레이스 페이지 css
 const Wrapper = styled.div`
@@ -19,9 +19,11 @@ const Wrapper = styled.div`
 `;
 
 const Place = () => {
-  const [ss, setSs] = useState("");
+  const [ss, setSs] = useState('');
   const [places, setPlaces] = useState([]);
   const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const SIZE = 12;
 
   useEffect(() => {
     axios
@@ -31,7 +33,9 @@ const Place = () => {
       });
   }, []);
 
-  // 이벤트 등록
+  if (!places) return null;
+
+  // 검색 이벤트 등록
   const handleSearchInput = (ss) => {
     const filteredPlaces = places.filter((place) => {
       return place.name.indexOf(ss) !== -1;
@@ -40,14 +44,33 @@ const Place = () => {
     setSs(ss);
   };
 
+  // 페이징 이벤트 등록
+  const handlePageChange = (pageNum) => {
+    setCurrentPage(pageNum);
+    const fetchNextStories = () => {
+      axios
+        .get(
+          `${process.env.REACT_APP_SERVER_URL}/api/place?page=${pageNum}&size=${SIZE}`
+        )
+        .then((response) => setPlaces(response.data));
+    };
+    fetchNextStories();
+  };
+
   return (
     <Wrapper>
       <Header />
-      <div className="title">
+      <div className='title'>
         <h1>에코 플레이스 둘러보기</h1>
       </div>
       <PlaceSearch ss={ss} onSearchInput={handleSearchInput} />
       <PlaceList places={places} filteredPlaces={filteredPlaces} ss={ss} />
+      <Pagination
+        items={places}
+        size={SIZE}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
       <Footer />
     </Wrapper>
   );
